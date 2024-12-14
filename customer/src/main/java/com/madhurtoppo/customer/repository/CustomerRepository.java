975267e0832601515c83b35e5ab94b41f919affb
Madhur Toppo <mtoppo@k15t.com>
2024-12-14 10:52:51 +0100

    Add event driven microservices

---
+++ customer/src/main/java/com/madhurtoppo/customer/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.madhurtoppo.customer.repository;
+
+import com.madhurtoppo.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    Optional<Customer> findByMobileNumberAndActiveSw(String mobileNumber,boolean active);
+
+
+}
---
+++ loans/src/main/java/com/madhurtoppo/loans/repository/LoansRepository.java
@@ -0,0 +1,14 @@
+package com.madhurtoppo.loans.repository;
+
+import com.madhurtoppo.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumberAndActiveSw(String mobileNumber, boolean activeSw);
+
+}
