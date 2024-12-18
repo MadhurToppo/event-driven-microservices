9b9c4a405a4f9153b67f1aa791c8bbe7dad2a057
Madhur Toppo <mtoppo@k15t.com>
2024-12-18 15:03:52 +0100

    Add command classes

---
+++ customer/src/main/java/com/madhurtoppo/customer/command/DeleteCustomerCommand.java
@@ -0,0 +1,16 @@
+package com.madhurtoppo.customer.command;
+
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+
+@Data
+@Builder
+public class DeleteCustomerCommand {
+
+    @TargetAggregateIdentifier
+    private final String customerId;
+    private final boolean isActive;
+
+}
