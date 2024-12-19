ada0f4f1c88486dedb39f8938c6886b2ff4d3336
Madhur Toppo <mtoppo@k15t.com>
2024-12-19 10:41:50 +0100

    Add Event Processors

--- customer/src/main/java/com/madhurtoppo/customer/CustomersApplication.java
+++ customer/src/main/java/com/madhurtoppo/customer/CustomersApplication.java
@@ -2,11 +2,14 @@ package com.madhurtoppo.customer;
 
 import com.madhurtoppo.customer.command.interceptor.CustomerCommandInterceptor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
@@ -16,8 +19,17 @@ public class CustomersApplication {
         SpringApplication.run(CustomersApplication.class, args);
     }
 
+
     @Autowired
     public void registerCustomerCommandInterceptor(ApplicationContext context, CommandGateway commandGateway) {
         commandGateway.registerDispatchInterceptor(context.getBean(CustomerCommandInterceptor.class));
     }
+
+
+    @Autowired
+    public void configure(EventProcessingConfigurer configurer) {
+        configurer.registerListenerInvocationErrorHandler("customer-group",
+                configuration -> PropagatingErrorHandler.instance());
+    }
+
 }
