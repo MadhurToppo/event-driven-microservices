4bec3b554623a7ec80c6a13331327b5dbc7a259f
Madhur Toppo <madhurtoppo@Madhurs-Mini.fritz.box>
2025-05-21 14:15:41 +0200

    Add success and failure meessage for Saga

--- customer/src/main/java/com/madhurtoppo/customer/command/controller/CustomerCommandController.java
+++ customer/src/main/java/com/madhurtoppo/customer/command/controller/CustomerCommandController.java
@@ -8,10 +8,18 @@ import com.madhurtoppo.customer.command.UpdateCustomerCommand;
 import com.madhurtoppo.customer.constants.CustomerConstants;
 import com.madhurtoppo.customer.dto.CustomerDto;
 import com.madhurtoppo.customer.dto.ResponseDto;
+import com.madhurtoppo.customer.query.FindCustomerQuery;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.SubscriptionQueryResult;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Nonnull;
 import java.util.UUID;
 
 
@@ -33,7 +42,7 @@ import java.util.UUID;
 public class CustomerCommandController {
 
     private final CommandGateway commandGateway;
-
+    private final QueryGateway queryGateway;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createCustomer(@Valid @RequestBody CustomerDto customerDto) {
@@ -79,21 +88,33 @@ public class CustomerCommandController {
                 .body(new ResponseDto(CustomerConstants.STATUS_200, CustomerConstants.MESSAGE_200));
     }
 
-
     @PatchMapping("/mobileNumber")
     public ResponseEntity<ResponseDto> updateMobileNumber(@Valid @RequestBody MobileNumberUpdateDto mobileNumberUpdateDto) {
-        UpdateCustomerMobileCommand updateCustomerMobileCommand = UpdateCustomerMobileCommand.builder()
+        UpdateCustomerMobileCommand updateCusMobNumCommand = UpdateCustomerMobileCommand.builder()
                 .customerId(mobileNumberUpdateDto.getCustomerId())
                 .accountNumber(mobileNumberUpdateDto.getAccountNumber())
                 .loanNumber(mobileNumberUpdateDto.getLoanNumber())
                 .cardNumber(mobileNumberUpdateDto.getCardNumber())
                 .mobileNumber(mobileNumberUpdateDto.getCurrentMobileNumber())
-                .newMobileNumber(mobileNumberUpdateDto.getNewMobileNumber())
-                .build();
-        commandGateway.sendAndWait(updateCustomerMobileCommand);
-        return ResponseEntity
-                .status(org.springframework.http.HttpStatus.OK)
-                .body(new ResponseDto(CustomerConstants.STATUS_200, CustomerConstants.MESSAGE_200));
+                .newMobileNumber(mobileNumberUpdateDto.getNewMobileNumber()).build();
+        try (SubscriptionQueryResult<ResponseDto, ResponseDto> queryResult = queryGateway.subscriptionQuery(
+                new FindCustomerQuery(mobileNumberUpdateDto.getNewMobileNumber()),
+                ResponseTypes.instanceOf(ResponseDto.class), ResponseTypes.instanceOf(ResponseDto.class))) {
+            commandGateway.send(updateCusMobNumCommand, new CommandCallback<>() {
+                @Override
+                public void onResult(@Nonnull CommandMessage<? extends UpdateCustomerMobileCommand> commandMessage,
+                                     @Nonnull CommandResultMessage<?> commandResultMessage) {
+                    if (commandResultMessage.isExceptional()) {
+                        ResponseEntity
+                                .status(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR)
+                                .body(new ResponseDto(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR.name(),
+                                        commandResultMessage.exceptionResult().getMessage()));
+                    }
+                }
+            });
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(queryResult.updates().blockFirst());
+        }
     }
 
 }
--- customer/src/main/java/com/madhurtoppo/customer/saga/UpdateMobileNumberSaga.java
+++ customer/src/main/java/com/madhurtoppo/customer/saga/UpdateMobileNumberSaga.java
@@ -14,6 +14,9 @@ import com.madhurtoppo.common.event.CardMobileUpdatedEvent;
 import com.madhurtoppo.common.event.CustomerMobileRollbackedEvent;
 import com.madhurtoppo.common.event.CustomerMobileUpdatedEvent;
 import com.madhurtoppo.common.event.LoanMobileUpdatedEvent;
+import com.madhurtoppo.customer.constants.CustomerConstants;
+import com.madhurtoppo.customer.dto.ResponseDto;
+import com.madhurtoppo.customer.query.FindCustomerQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.commandhandling.CommandCallback;
 import org.axonframework.commandhandling.CommandMessage;
@@ -22,10 +25,12 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.modelling.saga.EndSaga;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.axonframework.spring.stereotype.Saga;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Nonnull;
+
 
 
 @Saga
@@ -35,142 +40,130 @@ public class UpdateMobileNumberSaga {
     @Autowired
     private transient CommandGateway commandGateway;
 
+    @Autowired
+    private transient QueryUpdateEmitter queryUpdateEmitter;
 
     @StartSaga
     @SagaEventHandler(associationProperty = "customerId")
-    public void handle(CustomerMobileUpdatedEvent customerMobileUpdatedEvent) {
-        log.info("Saga Event 1 [Start] : Received customerMobileUpdatedEvent for customerId: {}",
-                customerMobileUpdatedEvent.getCustomerId());
-        UpdateAccountMobileCommand updateAccountMobileCommand = UpdateAccountMobileCommand.builder()
-                .accountNumber(customerMobileUpdatedEvent.getAccountNumber())
-                .cardNumber(customerMobileUpdatedEvent.getCardNumber())
-                .loanNumber(customerMobileUpdatedEvent.getLoanNumber())
-                .mobileNumber(customerMobileUpdatedEvent.getMobileNumber())
-                .newMobileNumber(customerMobileUpdatedEvent.getNewMobileNumber())
-                .build();
-        commandGateway.send(updateAccountMobileCommand, new CommandCallback<>() {
+    public void handle(CustomerMobileUpdatedEvent event) {
+        log.info("Saga Event 1 [Start] : Received CusMobNumUpdatedEvent for customerId: {}", event.getCustomerId());
+                UpdateAccountMobileCommand command = UpdateAccountMobileCommand.builder()
+                .accountNumber(event.getAccountNumber())
+                .cardNumber(event.getCardNumber())
+                .loanNumber(event.getLoanNumber())
+                .customerId(event.getCustomerId())
+                .mobileNumber(event.getMobileNumber())
+                .newMobileNumber(event.getNewMobileNumber()).build();
+        commandGateway.send(command, new CommandCallback<>() {
             @Override
             public void onResult(@Nonnull CommandMessage<? extends UpdateAccountMobileCommand> commandMessage,
-                    @Nonnull CommandResultMessage<?> commandResultMessage) {
+                                 @Nonnull CommandResultMessage<?> commandResultMessage) {
                 if (commandResultMessage.isExceptional()) {
-                    RollbackCustomerMobileCommand rollbackCustomerMobileCommand = RollbackCustomerMobileCommand.builder()
-                            .customerId(customerMobileUpdatedEvent.getCustomerId())
-                            .mobileNumber(customerMobileUpdatedEvent.getMobileNumber())
-                            .newMobileNumber(customerMobileUpdatedEvent.getNewMobileNumber())
-                            .errorMsg(commandResultMessage.exceptionResult().getMessage())
-                            .build();
-                    commandGateway.send(rollbackCustomerMobileCommand);
+                    RollbackCustomerMobileCommand rollbackCommand = RollbackCustomerMobileCommand.builder()
+                            .customerId(event.getCustomerId())
+                            .mobileNumber(event.getMobileNumber())
+                            .newMobileNumber(event.getNewMobileNumber())
+                            .errorMsg(commandResultMessage.exceptionResult().getMessage()).build();
+                    commandGateway.sendAndWait(rollbackCommand);
                 }
             }
         });
+
     }
 
-
     @SagaEventHandler(associationProperty = "customerId")
-    public void handle(AccountMobileUpdatedEvent accountMobileUpdatedEvent) {
-        log.info("Saga Event 2 [Start] : Received accountMobileUpdatedEvent for accountNumber: {}",
-                accountMobileUpdatedEvent.getAccountNumber());
-        UpdateCardMobileCommand updateCardMobileCommand = UpdateCardMobileCommand.builder()
-                .accountNumber(accountMobileUpdatedEvent.getAccountNumber())
-                .cardNumber(accountMobileUpdatedEvent.getCardNumber())
-                .loanNumber(accountMobileUpdatedEvent.getLoanNumber())
-                .customerId(accountMobileUpdatedEvent.getCustomerId())
-                .mobileNumber(accountMobileUpdatedEvent.getMobileNumber())
-                .newMobileNumber(accountMobileUpdatedEvent.getNewMobileNumber())
-                .build();
-        commandGateway.send(updateCardMobileCommand, new CommandCallback<>() {
+    public void handle(AccountMobileUpdatedEvent event) {
+        log.info("Saga Event 2 : Received AccntMobileNumUpdatedEvent for accountNumber: {}", event.getAccountNumber());
+        UpdateCardMobileCommand command = UpdateCardMobileCommand.builder()
+                .accountNumber(event.getAccountNumber())
+                .cardNumber(event.getCardNumber())
+                .loanNumber(event.getLoanNumber())
+                .customerId(event.getCustomerId())
+                .mobileNumber(event.getMobileNumber())
+                .newMobileNumber(event.getNewMobileNumber()).build();
+        commandGateway.send(command, new CommandCallback<>() {
             @Override
             public void onResult(@Nonnull CommandMessage<? extends UpdateCardMobileCommand> commandMessage,
-                    @Nonnull CommandResultMessage<?> commandResultMessage) {
+                                 @Nonnull CommandResultMessage<?> commandResultMessage) {
                 if (commandResultMessage.isExceptional()) {
-                    RollbackAccountMobileCommand rollbackAccountMobileCommand = RollbackAccountMobileCommand.builder()
-                            .accountNumber(accountMobileUpdatedEvent.getAccountNumber())
-                            .customerId(accountMobileUpdatedEvent.getCustomerId())
-                            .mobileNumber(accountMobileUpdatedEvent.getMobileNumber())
-                            .newMobileNumber(accountMobileUpdatedEvent.getNewMobileNumber())
-                            .errorMsg(commandResultMessage.exceptionResult().getMessage())
-                            .build();
-                    commandGateway.send(rollbackAccountMobileCommand);
+                    RollbackAccountMobileCommand rollbackCommand = RollbackAccountMobileCommand.builder()
+                            .accountNumber(event.getAccountNumber())
+                            .customerId(event.getCustomerId())
+                            .mobileNumber(event.getMobileNumber())
+                            .newMobileNumber(event.getNewMobileNumber())
+                            .errorMsg(commandResultMessage.exceptionResult().getMessage()).build();
+                    commandGateway.sendAndWait(rollbackCommand);
                 }
             }
         });
     }
 
-
     @SagaEventHandler(associationProperty = "customerId")
-    public void handle(CardMobileUpdatedEvent cardMobileUpdatedEvent) {
-        log.info("Saga Event 3 [Start] : Received cardMobileUpdatedEvent for cardNumber: {}",
-                cardMobileUpdatedEvent.getCardNumber());
-        UpdateLoanMobileCommand updateLoanMobileCommand = UpdateLoanMobileCommand.builder()
-                .customerId(cardMobileUpdatedEvent.getCustomerId())
-                .accountNumber(cardMobileUpdatedEvent.getAccountNumber())
-                .loanNumber(cardMobileUpdatedEvent.getLoanNumber())
-                .cardNumber(cardMobileUpdatedEvent.getCardNumber())
-                .mobileNumber(cardMobileUpdatedEvent.getMobileNumber())
-                .newMobileNumber(cardMobileUpdatedEvent.getNewMobileNumber())
-                .build();
-        commandGateway.send(updateLoanMobileCommand, new CommandCallback<>() {
+    public void handle(CardMobileUpdatedEvent event) {
+        log.info("Saga Event 3 : Received CardMobileNumUpdatedEvent for cardNumber: {}", event.getCardNumber());
+        UpdateLoanMobileCommand command = UpdateLoanMobileCommand.builder()
+                .accountNumber(event.getAccountNumber())
+                .cardNumber(event.getCardNumber())
+                .loanNumber(event.getLoanNumber())
+                .customerId(event.getCustomerId())
+                .mobileNumber(event.getMobileNumber())
+                .newMobileNumber(event.getNewMobileNumber()).build();
+        commandGateway.send(command, new CommandCallback<>() {
             @Override
             public void onResult(@Nonnull CommandMessage<? extends UpdateLoanMobileCommand> commandMessage,
-                    @Nonnull CommandResultMessage<?> commandResultMessage) {
+                                 @Nonnull CommandResultMessage<?> commandResultMessage) {
                 if (commandResultMessage.isExceptional()) {
-                    RollbackCardMobileCommand rollbackCardMobileCommand = RollbackCardMobileCommand.builder()
-                            .cardNumber(cardMobileUpdatedEvent.getCardNumber())
-                            .accountNumber(cardMobileUpdatedEvent.getAccountNumber())
-                            .customerId(cardMobileUpdatedEvent.getCustomerId())
-                            .mobileNumber(cardMobileUpdatedEvent.getMobileNumber())
-                            .newMobileNumber(cardMobileUpdatedEvent.getNewMobileNumber())
-                            .errorMsg(commandResultMessage.exceptionResult().getMessage())
-                            .build();
-                    commandGateway.send(rollbackCardMobileCommand);
+                    RollbackCardMobileCommand rollbackCommand = RollbackCardMobileCommand.builder()
+                            .cardNumber(event.getCardNumber())
+                            .accountNumber(event.getAccountNumber())
+                            .customerId(event.getCustomerId())
+                            .mobileNumber(event.getMobileNumber())
+                            .newMobileNumber(event.getNewMobileNumber())
+                            .errorMsg(commandResultMessage.exceptionResult().getMessage()).build();
+                    commandGateway.sendAndWait(rollbackCommand);
                 }
             }
         });
     }
 
+    @EndSaga
+    @SagaEventHandler(associationProperty = "customerId")
+    public void handle(LoanMobileUpdatedEvent event) {
+        log.info("Saga Event 4 [END] : Received LoanMobileNumUpdatedEvent for loanNumber: {}", event.getLoanNumber());
+        queryUpdateEmitter.emit(FindCustomerQuery.class, query -> true,
+                new ResponseDto(CustomerConstants.STATUS_200, CustomerConstants.MOBILE_UPD_SUCCESS_MESSAGE));
+    }
+
+    @SagaEventHandler(associationProperty = "customerId")
+    public void handle(CardMobileRollbackedEvent event) {
+        log.info("Saga Compensation Event : Received CardMobNumRollbackedEvent for cardNumber: {}", event.getCardNumber());
+        RollbackAccountMobileCommand rollbackCommand = RollbackAccountMobileCommand.builder()
+                .accountNumber(event.getAccountNumber())
+                .customerId(event.getCustomerId())
+                .mobileNumber(event.getMobileNumber())
+                .newMobileNumber(event.getNewMobileNumber())
+                .errorMsg(event.getErrorMsg()).build();
+        commandGateway.send(rollbackCommand);
+    }
+
+    @SagaEventHandler(associationProperty = "customerId")
+    public void handle(AccountMobileRollbackedEvent event) {
+        log.info("Saga Compensation Event : Received AccntMobNumRollbackedEvent for accountNumber: {}", event.getAccountNumber());
+        RollbackCustomerMobileCommand rollbackCommand = RollbackCustomerMobileCommand.builder()
+                .customerId(event.getCustomerId())
+                .mobileNumber(event.getMobileNumber())
+                .newMobileNumber(event.getNewMobileNumber())
+                .errorMsg(event.getErrorMsg()).build();
+        commandGateway.send(rollbackCommand);
+    }
 
     @EndSaga
     @SagaEventHandler(associationProperty = "customerId")
-    public void handle(LoanMobileUpdatedEvent loanMobileUpdatedEvent) {
-        log.info("Saga Event 4 [End] : Received loanMobileUpdatedEvent for loanNumber: {}",
-                loanMobileUpdatedEvent.getLoanNumber());
-    }
-
-
-    @SagaEventHandler(associationProperty = "customerId")
-    public void handle(CardMobileRollbackedEvent cardMobileRollbackedEvent) {
-        log.info("Saga Compensation Event : Received cardMobileRollbackedEvent for cardNumber: {}",
-                cardMobileRollbackedEvent.getCardNumber());
-        RollbackAccountMobileCommand rollbackAccountMobileCommand = RollbackAccountMobileCommand.builder()
-                .accountNumber(cardMobileRollbackedEvent.getAccountNumber())
-                .customerId(cardMobileRollbackedEvent.getCustomerId())
-                .mobileNumber(cardMobileRollbackedEvent.getMobileNumber())
-                .newMobileNumber(cardMobileRollbackedEvent.getNewMobileNumber())
-                .errorMsg(cardMobileRollbackedEvent.getErrorMsg())
-                .build();
-        commandGateway.send(rollbackAccountMobileCommand);
-    }
-
-
-    @SagaEventHandler(associationProperty = "customerId")
-    public void handle(AccountMobileRollbackedEvent accountMobileRollbackedEvent) {
-        log.info("Saga Compensation Event : Received accountMobileRollbackedEvent for accountNumber: {}",
-                accountMobileRollbackedEvent.getAccountNumber());
-        RollbackCustomerMobileCommand rollbackCustomerMobileCommand = RollbackCustomerMobileCommand.builder()
-                .customerId(accountMobileRollbackedEvent.getCustomerId())
-                .mobileNumber(accountMobileRollbackedEvent.getMobileNumber())
-                .newMobileNumber(accountMobileRollbackedEvent.getNewMobileNumber())
-                .errorMsg(accountMobileRollbackedEvent.getErrorMsg())
-                .build();
-        commandGateway.send(rollbackCustomerMobileCommand);
-    }
-
-
-    @EndSaga
-    @SagaEventHandler(associationProperty = "customerId")
-    public void handle(CustomerMobileRollbackedEvent customerMobileRollbackedEvent) {
-        log.info("Saga Compensation Event [End] : Received customerMobileRollbackedEvent for customerId: {}",
-                customerMobileRollbackedEvent.getCustomerId());
+    public void handle(CustomerMobileRollbackedEvent event) {
+        log.info("Saga Compensation Event [END] : Received CusMobNumRollbackedEvent for customerId: {}",
+                event.getCustomerId());
+        queryUpdateEmitter.emit(FindCustomerQuery.class, query -> true,
+                new ResponseDto(CustomerConstants.STATUS_500, CustomerConstants.MOBILE_UPD_FAILURE_MESSAGE));
     }
 
 }
